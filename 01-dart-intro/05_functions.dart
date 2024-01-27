e442c762298a3fb2bf796af1f8dd35a0f2be627d
AlexGrim <alejandrogrim12@gmail.com>
2024-01-26 20:41:41 -0600

    seccion 1

---
+++ 01-dart-intro/05_functions.dart
@@ -0,0 +1,21 @@
+void main() {
+  print(greetEveryone());
+  print('Suma: ${addTwoNumbers(10, 20)}');
+  print('Suma Opcional: ${addTwoNumbersOptional(10, 20)}');
+  print(greetPerson(name: 'Alex', message: 'Buenas tardes'));
+}
+
+String greetEveryone() => 'Hello everyone!';
+
+int addTwoNumbers(int a, int b) => (a + b);
+int addTwoNumbersOptional(int a, [int b = 0]) {
+
+  //   b ??= 0;
+
+  return (a + b);
+}
+
+
+String greetPerson({required String name, String? message = 'Hola'}){
+  return '$message, $name';
+}
