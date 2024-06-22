fb7bdbb56b550512256354632ab0115309b5f19f
AlexGrim <alejandrogrim12@gmail.com>
2024-06-22 13:28:01 -0600

    Remove unused files and configurations

---
+++ toktik/lib/domain/repositories/video_post_repository.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:toktik/domain/entities/video_post.dart';
+
+abstract class VideoPostRepository {
+  Future<List<VideoPost>> getFavoriteVideosByPage(String userId);
+  Future<List<VideoPost>> getTrendingVideosByPage(int page);
+}
